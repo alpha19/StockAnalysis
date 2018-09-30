ab553646772e314d0009c2ff0e2d2144088926c5
kdedow <kdedow@gmail.com>
2018-09-29 21:01:33 -0700

    Actually sent an email wooo

--- core/email.py
+++ core/email.py
@@ -1,5 +1,9 @@
 import os
+
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -11,9 +15,13 @@ class Email(smtplib.SMTP_SSL):
     This class provides a template from which an email can be sent.
     Gmail is used as the backend database
     """
+    RECIPIENTS = ["kdedow@gmail.com"]
 
     def __init__(self):
         smtplib.SMTP_SSL.__init__(self, 'smtp.gmail.com', 465)
+
+        self.message = MIMEMultipart('alternative')
+        self.message['To'] = ", ".join(Email.RECIPIENTS)
 
         self.ehlo()
         self.__login()
@@ -38,19 +46,38 @@ class Email(smtplib.SMTP_SSL):
             cipher_text = encrypt_file.read()
 
         plain_text = private_key.decrypt(cipher_text, padding.PKCS1v15()).decode('UTF-8')
+        login_creds = [text.strip() for text in plain_text.splitlines()]
+
+        if len(login_creds) == 2:
+            user = login_creds[0]
+            password = login_creds[1]
+            # Add to the email message
+            self.message['From'] = user
+        else:
+            # TODO: Log a message
+            pass
 
         # Login
+        self.login(user, password)
 
-    def setMessageBody(self, message: str):
-        pass
+    def addTextMessage(self, message: str):
+        text = MIMEText(message, 'plain')
+        self.message.attach(text)
+
+    def addHTMLMessage(self, message: str):
+        html = MIMEText(message, 'html')
+        self.message.attach(html)
 
     def setRecipients(self, recipients):
+        # Not planning on using this method since recipients is limited amount of people.
+        # If this does ever need to be implemented, email recipients should be managed in
+        # a database and this method should query database to get list of recipients
         pass
 
     def setSubject(self, subject: str):
-        pass
+        self.message['Subject'] = subject
 
     def sendMessage(self):
-        pass
+        self.sendmail(self.message.get('From'), self.message.get('To'), self.message.as_string())
 
 
--- cron_main.py
+++ cron_main.py
@@ -1,12 +1,22 @@
 from core.database_connection import Database
 from core.security_manager import SecurityManager
+from core.email import Email
 
 __author__ = 'kdedow'
 
 def main():
     with Database("../stocks.db") as db:
         analysisObj = SecurityManager(db)
+        # First update the stock table
         analysisObj.updateStocks()
+
+        # Now email the updated table to interested recipients
+        # TODO: Format the actual email body and subject with the tracked stock table!!!
+        email = Email()
+        email.setSubject("This is a subject")
+        email.addTextMessage("This is the message")
+
+        email.sendMessage()
 
 if __name__ == "__main__":
     main()
