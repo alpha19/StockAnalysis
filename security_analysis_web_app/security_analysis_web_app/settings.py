3c35c0d9288703d7d2572322c07312c0735ae9d6
kdedow <kdedow@gmail.com>
2019-05-27 20:18:04 -0700

    Basic user login mechanism

--- security_analysis_web_app/security_analysis_web_app/settings.py
+++ security_analysis_web_app/security_analysis_web_app/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'security_analysis_web_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,6 +112,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 
 # Static files (CSS, JavaScript, Images)
