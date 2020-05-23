f5563683296e696731756d4b9af553b8af87ec7b
kdedow <kdedow@gmail.com>
2020-05-23 14:57:46 -0700

    Starting to track stocks in django project

--- security_analysis_web_app/security_analysis_web_app/settings.py
+++ security_analysis_web_app/security_analysis_web_app/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/users/stocks'
+LOGIN_REDIRECT_URL = '/users/home'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'home'
 
--- security_analysis_web_app/user_accounts/models.py
+++ security_analysis_web_app/user_accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from stock_management.models import Stock
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    stocks = models.ManyToManyField(Stock)
+
+#    def __str__(self):
+#        return user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
