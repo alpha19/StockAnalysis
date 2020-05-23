8f78b585d412b5951a251a07570728c8227f2d30
kdedow <kdedow@gmail.com>
2020-05-23 14:57:46 -0700

    Starting to track stocks in django project

--- security_analysis_web_app/user_accounts/urls.py
+++ security_analysis_web_app/user_accounts/urls.py
@@ -1,9 +1,11 @@
 # accounts/urls.py
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('home/', views.user_home, name='user_home')
 ]
--- security_analysis_web_app/user_accounts/views.py
+++ security_analysis_web_app/user_accounts/views.py
@@ -3,6 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from django.contrib.auth.models import User
+
+from django.shortcuts import render
+
+def user_home(request):
+
+    user = request.user
+    num_stocks = user.profile.stocks.count()
+    context = {
+        'user_name': user.username,
+        'num_stocks' : num_stocks,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'user_page.html', context=context)
+
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
