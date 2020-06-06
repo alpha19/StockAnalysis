845012f4b942d300a9b1c0e4c28730c45bf7f0c6
kdedow <kdedow@gmail.com>
2020-06-06 14:29:46 -0700

    Continued work relating to adding a new stock - NOT TESTED

--- security_analysis_web_app/user_accounts/views.py
+++ security_analysis_web_app/user_accounts/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-from django.contrib.auth.models import User
-from stock_management.models import Stock
+from ..stock_management.models import Stock
+from ..stock_management.forms import NewStockForm
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def user_home(request):
 
@@ -32,5 +33,39 @@ class UserStockList(generic.ListView):
     template_name = "user_stock_list.html"
 
     def get_queryset(self):
-        #checked_objects.definitions.all()
         return self.request.user.profile.stocks.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(UserStockList, self).get_context_data(**kwargs)
+        context['form'] = NewStockForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        # Perform logic to add the new symbol
+        tickerForm = NewStockForm(request.post)
+        if tickerForm.is_valid():
+            tickerForm.update()
+            user = request.user
+
+            # Update the list of user held stocks
+            stock = Stock.objects.get(pk=tickerForm.ticker)
+
+            # 1. If stock not part of list of user stocks, then add
+            # 2. If stock is part of user stocks, then update with latest data
+            found = False
+            for userStock in user.profile.stocks:
+                if userStock.ticker_symbol == stock.ticker_symbol:
+                    userStock.refresh_from_db()
+                    found = True
+                    break
+
+            if not found:
+                user.profile.stocks.add(stock)
+
+        # Render an updated page. Right now just keep the status quo
+
+        # TODO: Add a success or failure method
+
+        # TODO: Add a remove button
+
+        return self.get(request=request, *args, **kwargs)
