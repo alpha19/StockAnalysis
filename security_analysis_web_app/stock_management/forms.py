eba8d9064da9b67cb614a9fc7175b64e3545fc43
kdedow <kdedow@gmail.com>
2020-07-03 14:58:46 -0700

    Update and add stocks initial implementaiton

--- security_analysis_web_app/stock_management/forms.py
+++ security_analysis_web_app/stock_management/forms.py
@@ -1,52 +1,19 @@
 from django import forms
 
-from .models import Stock
-import keys
-
-import yfinance as yf
-import finnhub
-from finnhub.rest import ApiException
+from .views import StockController
 
 class NewStockForm(forms.Form):
     ticker = forms.CharField(label='Ticker', max_length=10)
+    stockController = StockController
+
+    def __init__(self, request=None):
+        super(NewStockForm,self).__init__(request)
+
+        if request is not None:
+            self.is_valid()
+
+            self.ticker = self.cleaned_data['ticker']
+            self.stockController = StockController(self.ticker)
 
     def update(self):
-        self.ticker = self.cleaned_data['ticker']
-
-        # Check if in stocks db. If not, then create and update. Otherwise just update.
-        stock = None
-        try:
-            stock = Stock.objects.get(pk=self.ticker)
-        except Stock.DoesNotExist:
-            stock = Stock(ticker_symbol=self.ticker)
-        finally:
-            self.queryForNewData(stock)
-            stock.save()
-
-    # Updates the data via REST call for the given stock
-    def queryForNewData(self, stock):
-        stockQuery = yf.Ticker(stock.ticker_symbol)
-        finnData = self.queryQuote()
-
-        stock.price = finnData.c
-
-        # Fill out the rest of the required data
-
-        return
-
-    # Queries REST API
-    def getQuote(self):
-        # Configure API key
-        configuration = finnhub.Configuration( api_key = { 'token': keys.FINNHUB_KEY } )
-
-        with finnhub.ApiClient(configuration) as api_client:
-            # Create an instance of the API class
-            api_instance = finnhub.DefaultApi(api_client)
-
-            try:
-                # Aggregate Indicators
-                return api_instance.quote(self.ticker)
-            except ApiException as e:
-                print("Exception when calling DefaultApi->aggregate_indicator: %s\n" % e)
-
-        return None
+        self.stockController.update()
--- security_analysis_web_app/stock_management/views.py
+++ security_analysis_web_app/stock_management/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
 
+from .models import Stock
+
+import yfinance as yf
+import finnhub
+from finnhub.rest import ApiException
+
+import keys
+
 # Create your views here.
+
+# This is basically a controller. Does it belong here???
+class StockController:
+    stock = Stock()
+
+    def __init__(self, ticker: str):
+        self.getStockObject(ticker)
+
+    def getStockObject(self, ticker):
+        try:
+            self.stock = Stock.objects.get(pk=ticker)
+        except Stock.DoesNotExist:
+            self.stock = Stock(ticker_symbol=ticker)
+
+    def update(self):
+        self.queryForNewData()
+        self.stock.save()
+
+    # Updates the data via REST call for the given stock
+    def queryForNewData(self):
+        stockQuery = yf.Ticker(self.stock.ticker_symbol)
+        finnData = self.getQuote()
+
+        self.stock.price = finnData.c
+
+        # Fill out the rest of the required data
+        self.stock.ticker_symbol = stockQuery.info['symbol']
+        self.stock.company_name = stockQuery.info['longName']
+        self.stock.daily_change = finnData.c - finnData.o
+        self.stock.daily_percent = 100 * (self.stock.daily_change / finnData.o)
+        self.stock.year_high = stockQuery.info['fiftyTwoWeekHigh']
+        self.stock.year_low = stockQuery.info['fiftyTwoWeekLow']
+
+        # TODO: Is this still even relevant/worth it?
+        self.stock.streak = 0
+
+        return
+
+    # Queries REST API
+    def getQuote(self):
+        # Configure API key
+        configuration = finnhub.Configuration( api_key = { 'token': keys.FINNHUB_KEY } )
+
+        with finnhub.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = finnhub.DefaultApi(api_client)
+
+            try:
+                # Aggregate Indicators
+                return api_instance.quote(self.stock.ticker_symbol)
+            except ApiException as e:
+                print("Exception when calling DefaultApi->aggregate_indicator: %s\n" % e)
+
+        return None
